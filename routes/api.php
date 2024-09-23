a6d7f98817d82d282099c5ad85a50c35ca174cb8
Keen <keenvergara@gmail.com>
2024-09-24 04:06:50 +0800

    feat: added tenancy

--- routes/api.php
+++ routes/api.php
@@ -4,16 +4,22 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\Auth\RegistrationController;
 use App\Http\Controllers\Auth\ForgotPasswordController;
+use Stancl\Tenancy\Middleware\InitializeTenancyByDomain;
+use Stancl\Tenancy\Middleware\PreventAccessFromCentralDomains;
 
-Route::post('login', [LoginController::class, 'store'])->name('authenticate.login');
-Route::post('register', [RegistrationController::class, 'store'])->name('authenticate.register');
-Route::post('password/forgot', [ForgotPasswordController::class, 'store'])->name('authenticate.forgot');
-Route::patch('password/reset/{user}', [ForgotPasswordController::class, 'update'])->name('authenticate.reset')->middleware('signed');
+foreach (config('tenancy.central_domains') as $domain) {
+    Route::domain($domain)->group(function () {
+        //
+    });
+};
 
-Route::middleware('auth:sanctum')->group(function () {
-    Route::delete('logout', [LoginController::class, 'destroy'])->name('authenticate.logout');
-});
+Route::middleware(['api', InitializeTenancyByDomain::class, PreventAccessFromCentralDomains::class])->group(function () {
+    Route::post('login', [LoginController::class, 'store'])->name('authenticate.login');
+    Route::post('register', [RegistrationController::class, 'store'])->name('authenticate.register');
+    Route::post('password/forgot', [ForgotPasswordController::class, 'store'])->name('authenticate.forgot');
+    Route::patch('password/reset/{user}', [ForgotPasswordController::class, 'update'])->name('authenticate.reset')->middleware('signed');
 
-Route::group(['prefix' => 'v1'], function () {
-    //
+    Route::middleware('auth:sanctum')->group(function () {
+        Route::delete('logout', [LoginController::class, 'destroy'])->name('authenticate.logout');
+    });
 });

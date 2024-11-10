13f522f000557306eb4375846c476d46ae9f4859
Keen <keenvergara@gmail.com>
2024-11-11 02:16:22 +0800

    wip: added migration, mvc for chart of accounts

---
+++ app/Models/Tenants/Accounting/ChartOfAccount.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models\Tenants\Accounting;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class ChartOfAccount extends Model
+{
+    use HasFactory;
+}

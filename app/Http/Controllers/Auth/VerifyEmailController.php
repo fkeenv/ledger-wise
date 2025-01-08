9e057336813b0596a61998091cf1df543138a3ff
Keen <keenvergara@gmail.com>
2025-01-08 22:25:18 +0800

    feat: installed inertia for central domain

--- app/Http/Controllers/Auth/VerifyEmailController.php
+++ app/Http/Controllers/Auth/VerifyEmailController.php
@@ -2,48 +2,26 @@
 
 namespace App\Http\Controllers\Auth;
 
-use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\Auth\Events\Verified;
+use Illuminate\Foundation\Auth\EmailVerificationRequest;
+use Illuminate\Http\RedirectResponse;
 
 class VerifyEmailController extends Controller
 {
     /**
-     * Display a listing of the resource.
+     * Mark the authenticated user's email address as verified.
      */
-    public function index()
+    public function __invoke(EmailVerificationRequest $request): RedirectResponse
     {
-        //
-    }
+        if ($request->user()->hasVerifiedEmail()) {
+            return redirect()->intended(route('dashboard', absolute: false).'?verified=1');
+        }
 
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
-    {
-        //
-    }
+        if ($request->user()->markEmailAsVerified()) {
+            event(new Verified($request->user()));
+        }
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(string $id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, string $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(string $id)
-    {
-        //
+        return redirect()->intended(route('dashboard', absolute: false).'?verified=1');
     }
 }
--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,15 +2,8 @@
 
 namespace App\Providers;
 
-use Stancl\JobPipeline\JobPipeline;
-use Illuminate\Support\Facades\Event;
-use Stancl\Tenancy\Jobs\SeedDatabase;
-use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Vite;
 use Illuminate\Support\ServiceProvider;
-use Stancl\Tenancy\Jobs\CreateDatabase;
-use Stancl\Tenancy\Events\DomainCreated;
-use Stancl\Tenancy\Events\TenantCreated;
-use Stancl\Tenancy\Jobs\MigrateDatabase;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -27,16 +20,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        Event::listen(TenantCreated::class, JobPipeline::make([
-            CreateDatabase::class,
-            MigrateDatabase::class,
-            SeedDatabase::class,
-        ])->send(function (TenantCreated $event) {
-            return $event->tenant;
-        })->shouldBeQueued(true)->toListener());
-
-        Event::listen(DomainCreated::class, function () {
-            Artisan::call('queue:work --once');
-        });
+        Vite::prefetch(concurrency: 3);
     }
 }

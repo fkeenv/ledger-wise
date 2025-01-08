7f2bbbb24dc700093f1fbc85fa1510206a4323ea
Keen <keenvergara@gmail.com>
2025-01-08 22:25:18 +0800

    feat: installed inertia for central domain

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,53 @@
 <?php
 
+use Inertia\Inertia;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Foundation\Application;
+use App\Http\Controllers\ProfileController;
+use Stancl\Tenancy\Middleware\InitializeTenancyBySubdomain;
+use Stancl\Tenancy\Middleware\PreventAccessFromCentralDomains;
 
-Route::get('/', function () {
-    return view('welcome');
+foreach (config('tenancy.central_domains') as $domain) {
+    Route::domain($domain)->group(function () {
+        Route::get('/', function () {
+            return Inertia::render('Welcome', [
+                'canLogin' => Route::has('login'),
+                'canRegister' => Route::has('register'),
+                'laravelVersion' => Application::VERSION,
+                'phpVersion' => PHP_VERSION,
+            ]);
+        });
+
+        Route::get('/dashboard', function () {
+            return Inertia::render('Dashboard');
+        })->middleware(['auth', 'verified'])->name('dashboard');
+
+        Route::middleware('auth')->group(function () {
+            Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+            Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+            Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+        });
+    });
+}
+
+Route::middleware([
+    'web',
+    InitializeTenancyBySubdomain::class,
+    PreventAccessFromCentralDomains::class,
+])->group(function () {
+    Route::get('/', function () {
+        return 'Hello';
+    });
+
+    Route::get('/dashboard', function () {
+        return Inertia::render('Dashboard');
+    })->middleware(['auth', 'verified'])->name('tenant.dashboard');
+
+    Route::middleware('auth')->group(function () {
+        Route::get('/profile', [ProfileController::class, 'edit'])->name('tenant.profile.edit');
+        Route::patch('/profile', [ProfileController::class, 'update'])->name('tenant.profile.update');
+        Route::delete('/profile', [ProfileController::class, 'destroy'])->name('tenant.profile.destroy');
+    });
 });
+
+require __DIR__.'/auth.php';

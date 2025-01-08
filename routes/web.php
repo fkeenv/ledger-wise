e32e396b81f27fc4ca90318905afe79cc48ec9f1
Keen <keenvergara@gmail.com>
2025-01-08 23:30:39 +0800

    chore: removed tenant web routes

--- routes/web.php
+++ routes/web.php
@@ -4,8 +4,6 @@ use Inertia\Inertia;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Foundation\Application;
 use App\Http\Controllers\ProfileController;
-use Stancl\Tenancy\Middleware\InitializeTenancyBySubdomain;
-use Stancl\Tenancy\Middleware\PreventAccessFromCentralDomains;
 
 foreach (config('tenancy.central_domains') as $domain) {
     Route::domain($domain)->group(function () {
@@ -29,25 +27,5 @@ foreach (config('tenancy.central_domains') as $domain) {
         });
     });
 }
-
-Route::middleware([
-    'web',
-    InitializeTenancyBySubdomain::class,
-    PreventAccessFromCentralDomains::class,
-])->group(function () {
-    Route::get('/', function () {
-        return 'Hello';
-    });
-
-    Route::get('/dashboard', function () {
-        return Inertia::render('Dashboard');
-    })->middleware(['auth', 'verified'])->name('tenant.dashboard');
-
-    Route::middleware('auth')->group(function () {
-        Route::get('/profile', [ProfileController::class, 'edit'])->name('tenant.profile.edit');
-        Route::patch('/profile', [ProfileController::class, 'update'])->name('tenant.profile.update');
-        Route::delete('/profile', [ProfileController::class, 'destroy'])->name('tenant.profile.destroy');
-    });
-});
 
 require __DIR__.'/auth.php';
